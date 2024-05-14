48fce048b04909b3bed415a9d65ddf593c93fe58
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-05-14 11:39:29 -0300

    :sparkles: feat: add list user route

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,11 +2,13 @@ import fastify from 'fastify';
 
 import { createUser } from '@/routes/users/create-user';
 import { listUsers } from '@/routes/users/list-users';
+import { listUser } from '@/routes/users/list-user';
 
 const app = fastify();
 
 app.register(createUser);
 app.register(listUsers);
+app.register(listUser);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server is running...');
---
+++ src/routes/users/list-user.ts
@@ -0,0 +1,21 @@
+import { FastifyInstance } from 'fastify';
+import { prisma } from '@/lib/prisma';
+import z from 'zod';
+
+export async function listUser(app: FastifyInstance) {
+  app.get('/users/:id', async (request, reply) => {
+    const userParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = userParams.parse(request.params);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return reply.status(200).send(user);
+  });
+}
