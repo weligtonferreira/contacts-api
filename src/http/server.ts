a499f48324f8a37b0e7c4d365a2c9920a6eb5e62
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-05-14 12:13:06 -0300

    :sparkles: feat: add delete user route

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,6 +4,7 @@ import { createUser } from '@/routes/users/create-user';
 import { listUsers } from '@/routes/users/list-users';
 import { listUser } from '@/routes/users/list-user';
 import { updateUser } from '@/routes/users/update-user';
+import { deleteUser } from '@/routes/users/delete-user';
 
 const app = fastify();
 
@@ -11,6 +12,7 @@ app.register(createUser);
 app.register(listUsers);
 app.register(listUser);
 app.register(updateUser);
+app.register(deleteUser);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server is running...');
---
+++ src/routes/users/delete-user.ts
@@ -0,0 +1,26 @@
+import { FastifyInstance } from 'fastify';
+import z from 'zod';
+
+import { prisma } from '@/lib/prisma';
+
+export async function deleteUser(app: FastifyInstance) {
+  app.delete('/users/:id', async (request, reply) => {
+    const userParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = userParams.parse(request.params);
+
+    const user = await prisma.user.findUnique({ where: { id } });
+
+    if (user === null) {
+      return reply.status(404).send({ message: 'User not found' });
+    }
+
+    await prisma.user.delete({
+      where: { id },
+    });
+
+    return reply.status(200).send();
+  });
+}
