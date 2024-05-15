75bf1193dc3ad8698a807a5c16504da061126664
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-05-14 21:45:15 -0300

    :sparkles: feat: add configuration to use jwt as a plugin to authenticate routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify';
 
+import fastifyPlugin from './fastifyPlugin';
+
 import { authenticateUser } from '@/routes/users/authenticate-user';
 import { createUser } from '@/routes/users/create-user';
 import { listUsers } from '@/routes/users/list-users';
@@ -8,6 +10,8 @@ import { updateUser } from '@/routes/users/update-user';
 import { deleteUser } from '@/routes/users/delete-user';
 
 const app = fastify();
+
+app.register(fastifyPlugin);
 
 app.register(authenticateUser);
 app.register(createUser);
