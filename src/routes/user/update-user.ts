d575c7ef56d964a1dcf05395152e27e68cd18042
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-06-25 14:31:02 -0300

    :bug: fix: change the updateUser function

--- src/routes/user/update-user.ts
+++ src/routes/user/update-user.ts
@@ -4,36 +4,43 @@ import z from 'zod';
 
 import { prisma } from '@/lib/prisma';
 
+interface UpdatedUserProps {
+  name?: string;
+  email?: string;
+  password?: string;
+}
+
 export async function updateUser(app: FastifyInstance) {
-  app.put('/users/:id', { preValidation: [app.verifyToken] }, async (request, reply) => {
-    const updateUserBody = z.object({
-      name: z.string(),
-      email: z.string().email(),
-      password: z.string(),
-    });
+  app.put(
+    '/users/:id',
+    { preValidation: [app.verifyToken] },
+    async (request, reply) => {
+      const userParams = z.object({
+        id: z.string().uuid(),
+      });
 
-    const userParams = z.object({
-      id: z.string().uuid(),
-    });
+      const updatedUserData = request.body as UpdatedUserProps;
+      const { id } = userParams.parse(request.params);
 
-    const { name, email, password } = updateUserBody.parse(request.body);
-    const { id } = userParams.parse(request.params);
+      const user = await prisma.user.findUnique({ where: { id } });
 
-    const user = await prisma.user.findUnique({ where: { id } });
+      if (user === null) {
+        return reply.status(404).send({ message: 'User not found' });
+      }
 
-    if (user === null) {
-      return reply.status(404).send({ message: 'User not found' });
+      if (updatedUserData.password) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          8
+        );
+      }
+
+      const updatedUser = await prisma.user.update({
+        data: updatedUserData,
+        where: { id },
+      });
+
+      return reply.status(200).send({ updatedUser });
     }
-
-    const updatedUser = await prisma.user.update({
-      data: {
-        name,
-        email,
-        password: await bcrypt.hash(password, 8),
-      },
-      where: { id },
-    });
-
-    return reply.status(200).send({ updatedUser });
-  });
+  );
 }
